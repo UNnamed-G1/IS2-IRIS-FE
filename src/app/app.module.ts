bb1aea5e5e1af2d0a9ccb2340842805862d60d42
John Castro Hernandez <jfcastroh@unal.edu.co>
2018-03-14 20:57:57 -0500

    everything is ruined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
 import { AppServicesComponent } from './app-services/app-services.component';
+import { HomeComponent } from './home/home.component';
 
+
+export const appRoutes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    AppHeaderComponent,
-    AppFooterComponent,
-    AppServicesComponent
-  ],
+      AppComponent,
+      LoginComponent,
+      AppHeaderComponent,
+      AppFooterComponent,
+      AppServicesComponent,
+      HomeComponent
+
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( appRoutes )
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
