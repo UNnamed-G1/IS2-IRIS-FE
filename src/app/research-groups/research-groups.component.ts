cd08525a08a578c4b9d2cb2761566f237cb4ad32
John Castro Hernandez <jfcastroh@unal.edu.co>
2018-03-31 07:19:49 -0500

    research_groups, research_group services, CRUDS for research_groups

--- src/app/research-groups/research-groups.component.ts
+++ src/app/research-groups/research-groups.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ResearchGroupsService } from '../research-groups.service';
+import { Research_groups } from '../research_groups';
 @Component({
   selector: 'app-research-groups',
   templateUrl: './research-groups.component.html',
   styleUrls: ['./research-groups.component.css']
 })
 export class ResearchGroupsComponent implements OnInit {
+  columns = ['id','name','description','strategic_focus','reasearch_priorities','foundation_date','classification','date_classification','url'];
+  rows : Array<Research_groups>;
+  constructor(private researchGroupsService: ResearchGroupsService ,private  router : Router) {
+      }
+      ngOnInit() {
+        this.researchGroupsService.get("research_groups").subscribe((res : Research_groups[]) => {
+          console.log(res['research_groups'])
+          this.rows = res['research_groups'];
+        });
+      };
 
-  constructor() { }
+  public delete(id:string){
 
-  ngOnInit() {
+      console.log("delete : " + id);
+      var path = 'users/' + id;
+      }
+
+
+  public update(id:string){
+    console.log("update : " + id );
+    this.router.navigateByUrl('/users/add/' + id);
   }
-
 }
---
+++ src/app/research_groups.ts
@@ -0,0 +1,11 @@
+export class Research_groups{
+      public id:number;
+      public name:string;
+      public description:string;
+      public strategic_focus:string;
+      public reasearch_priorities:string;
+      public foundation_date:string;
+      public classification:string;
+      public date_classification:string;
+      public url:string;
+}
