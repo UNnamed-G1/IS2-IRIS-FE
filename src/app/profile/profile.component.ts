e9a8863a24afe76ac7351ee29921aea4ab6ffa0d
Julián Salomón <jesalomont@unal.edu.co>
2018-04-02 03:08:22 -0500

    + Profile view + New methods in user service for profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../users/user.service'
+import { select } from '@angular-redux/store';
+import { PermissionManager } from '../permission-manager';
+import { User } from '../users/user';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  @select() auxiliarID;
+  user: User;
+  showInput: boolean = false;
 
-  constructor() { }
+  constructor(private userService: UserService,
+    private permMan: PermissionManager) { }
 
   ngOnInit() {
+    if (this.permMan.validateLogged()) {
+      this.auxiliarID.subscribe(id => {
+        let getUser = (id) ? this.userService.getUser(id) : this.userService.getCurrentUser();
+        getUser.subscribe((user: {user: User}) => {
+          this.user = new User();
+          Object.assign(this.user, user.user);
+          console.log(this.user)
+        });
+      });
+    }
+  }
+
+  updateProfile() {
+    this.userService.updateUser(this.user.id, this.user).subscribe(
+      response => console.log(response),
+      error => console.error(error)
+    );
+    this.toggleShowInput();
+  }
+
+  toggleShowInput() {
+    this.showInput = !this.showInput
   }
 
 }
--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CommonService } from '../common.service'
 
 @Injectable()
 export class UserService {
-  
+
   API_URL: string = "http://localhost:3000/";
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient, private commonService: CommonService) { }
+
+  public getUser(id) {
+    return this.commonService.get("users/" + id);
+  }
+
+  public getCurrentUser() {
+    return this.commonService.get("users/current")
+  }
+
+  public updateUser(id, user) {
+    return this.commonService.update("users/" + id, user);
+  }
+
   // read method
   public get(path) {
 
