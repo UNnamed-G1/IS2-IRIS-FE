a4710f77ae097ad441966b9579ca52627fdc9b0d
Julián Salomón <jesalomont@unal.edu.co>
2018-04-02 22:14:23 -0500

    Merge fix

--- src/app/research-groups/research-groups.service.ts
+++ src/app/research-groups/research-groups.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import { ResearchGroup } from './research-groups';
+//import { ResearchGroup } from './research-group';
 
 @Injectable()
 export class ResearchGroupService {
