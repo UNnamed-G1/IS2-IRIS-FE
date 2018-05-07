0cbe60cef4c751d2d9f014ad8f1c2375fbe8832c
Julián Salomón <jesalomont@unal.edu.co>
2018-05-07 03:02:00 -0500

    * Refactoring on AddEvent component * Changed swal to GenericSwal component

--- src/app/professor/event-list/add/add-event.component.ts
+++ src/app/professor/event-list/add/add-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ViewChild, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { SwalComponent } from '@toverux/ngx-sweetalert2';
@@ -11,144 +11,155 @@ import { PermissionManager } from 'app/permission-manager';
 import { Event, ResearchGroup } from 'app/classes/_models';
 import { EventService } from 'app/services/event.service';
 import { UserService } from 'app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-event',
   templateUrl: './add-event.component.html',
   styleUrls: ['./add-event.component.css']
 })
-
-export class AddEventComponent implements OnInit, AfterContentInit, OnDestroy {
-  @ViewChild('sucSwal') private sucSwal: SwalComponent;
-  @ViewChild('warnSwal') private warnSwal: SwalComponent;
-  @ViewChild('errSwal') private errSwal: SwalComponent;
+export class AddEventComponent implements OnInit {
 
   @select(['auxiliarID', 'eventUpdate']) eventUpdateID;
   @select(['session', 'id']) userID;
-  rgsUser: Array<ResearchGroup>;
-  event: Event = new Event();
+
+  private eventId: number;
+  usrGroups: Array<ResearchGroup>;
   eventForm: FormGroup;
-  event_types: string[] = ['Publico', 'Privado'];
-  frequence_types: string[] = ['Unico', 'Repetitivo'];
-  state_types: string[] = ['Activo', 'Inactivo'];
-  minDate = new Date();         // Actual date
-  // initial center position for the map
+  types: { [Key: string]: string[] } = {
+    event: ['Publico', 'Privado'],
+    frequence: ['Unico', 'Repetitivo'],
+    state: ['Activo', 'Inactivo']
+  }
+  minDate: Date = new Date(); // Actual date for event date
+  swalOpts: any;
+  // Initial map position
   lat: number = 4.63858;
   lng: number = -74.0841;
 
-  constructor(private eventService: EventService,
+  constructor(
+    private eventService: EventService,
     private userService: UserService,
     private permMan: PermissionManager,
     private ngRedux: NgRedux<AppState>,
-    private formBuilder: FormBuilder) { }
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
-    this.permMan.validateSession(['Profesor']);
-    this.userID.subscribe((id) => {
-      this.userService.get(id).subscribe(
-        (response: any) => {
-          this.rgsUser = response.user.research_groups;
-        }
-      );
-    })
-  }
-
-  ngAfterContentInit() {
-    this.eventUpdateID.subscribe((id: number) => {
-      if (id) {
-        this.eventService.get(id).subscribe(
-          (response: { event: Event }) => {
-            this.event = response.event
-            this.event.research_group_id = this.event.research_group.id;
-            this.lat = this.event.latitude;
-            this.lng = this.event.longitude;
-            this.createEventForm();
+    this.createEventForm(new Event());
+    if (this.permMan.validateSession(['Profesor'])) {
+      this.userID.subscribe(id => {
+        this.userService.get(id).subscribe(
+          (response: any) => {
+            this.usrGroups = response.user.research_groups;
           },
           (error: HttpErrorResponse) => {
-            this.errSwal.title = 'No se ha podido obtener el evento';
-            this.errSwal.text = 'Mensaje de error: ' + error.message;
-            this.errSwal.show();
+            this.swalOpts = { title: 'No se ha podido obtener tus grupos de investigación', text: error.message, type: 'error' };
           }
         );
-      }
-    });
-    this.createEventForm();
-  }
-
-  ngOnDestroy() {
-    this.ngRedux.dispatch({ type: REMOVE_AUXILIAR, remove: 'eventUpdate' });
+      });
+      this.eventUpdateID.subscribe((id: number) => {
+        if (id) {
+          this.eventService.get(id).subscribe(
+            (response: { event: Event }) => {
+              this.eventId = response.event.id;
+              this.setEvent(response.event);
+            },
+            (error: HttpErrorResponse) => {
+              this.swalOpts = { title: 'No se ha podido obtener el evento', text: error.message, type: 'error' };
+            }
+          );
+        } else {
+          // No id stored
+          this.router.navigateByUrl('');
+        }
+      });
+    }
   }
 
   onSubmit() {
+    if (this.eventForm.invalid) {
+      this.swalOpts = { title: 'Hay datos inválidos', text: 'Por favor corrígelos y vuelve a intentarlo', type: 'warning' };
+      for (const k in this.eventForm.controls) {
+        if (this.eventForm.get(k).invalid) {
+          this.eventForm.get(k).markAsDirty;
+        }
+      }
+      return;
+    }
     if (this.eventForm.pristine) {
-      this.warnSwal.title = 'No has realizado cambios';
-      this.warnSwal.text = 'Los datos no serán actualizados';
-      this.warnSwal.show();
+      this.swalOpts = { title: 'No has realizado cambios', text: 'Los datos no serán actualizados', type: 'warning' };
       return;
     }
     const e = new Event();
     for (const k in this.eventForm.controls) {
       if (this.eventForm.get(k).dirty) {
         e[k] = this.eventForm.get(k).value;
+        if (k === 'event_type' || k === 'frequence' || k === 'state') {
+          e[k] = e[k].toLowerCase();
+        }
       }
     }
     if (e.event_type) {
-      e.type_ev = e.event_type.toLowerCase();
+      e.type_ev = e.event_type;
       delete e.event_type;
     }
-    if (e.frequence) {
-      e.frequence = e.frequence.toLowerCase();
-    }
-    if (e.state) {
-      e.state = e.state.toLowerCase();
-    }
-    if (this.event.id) {
-      this.eventService.update(this.event.id, { event: e }).subscribe(
+    if (this.eventId) {
+      this.eventService.update(this.eventId, { event: e }).subscribe(
         (response: { event: Event }) => {
-          this.sucSwal.title = 'El evento ha sido actualizado';
-          this.sucSwal.show();
-          Object.assign(this.event, response.event);
-          this.event.research_group_id = this.event.research_group.id;
-          this.createEventForm();
+          this.swalOpts = { title: 'El evento ha sido actualizado', type: 'success' };
+          this.setEvent(response.event);
         },
         (error: HttpErrorResponse) => {
-          this.errSwal.title = 'Evento no actualizado';
-          this.errSwal.text = 'Mensaje de error: ' + error.message;
-          this.errSwal.show();
+          this.swalOpts = { title: 'Evento no actualizado', text: error.message, type: 'error' };
         }
       );
     } else {
       this.eventService.create({ event: e }).subscribe(
         (response: { event: Event }) => {
-          this.sucSwal.title = 'El evento ha sido añadido';
-          this.sucSwal.show();
+          this.swalOpts = { title: 'El evento ha sido añadido', type: 'success' };
           this.eventForm.reset();
         },
         (error: HttpErrorResponse) => {
-          this.errSwal.title = 'Evento no añadido';
-          this.errSwal.text = 'Mensaje de error: ' + error.message;
-          this.errSwal.show();
+          this.swalOpts = { title: 'Evento no añadido', text: error.message, type: 'error' };
         }
       );
     }
   }
 
-  private createEventForm() {
+  setEvent(event: Event) {
+    event.research_group_id = event.research_group.id;
+    this.createEventForm(event);
+    this.lat = event.latitude;
+    this.lng = event.longitude;
+  }
+
+  // Maps events
+  setCoords($event: MouseEvent) {
+    this.latitude.setValue($event.coords.lat);
+    this.latitude.markAsDirty();
+    this.longitude.setValue($event.coords.lng);
+    this.longitude.markAsDirty();
+  }
+
+  private createEventForm(ev: Event) {
     this.eventForm = this.formBuilder.group({
-      name: [this.event.name, Validators.required],
-      topic: [this.event.topic, [Validators.required, Validators.maxLength(5000)]],
-      description: [this.event.description, [Validators.required, Validators.maxLength(5000)]],
-      event_type: [this.event.event_type, Validators.required],
-      date: [this.event.date, Validators.required],
-      research_group_id: [this.event.research_group_id, Validators.required],
-      frequence: [this.event.frequence, Validators.required],
-      duration: [this.event.duration, Validators.required],
-      state: [this.event.state, Validators.required],
-      address: [this.event.address, Validators.required],
-      latitude: [this.event.latitude],
-      longitude: [this.event.longitude],
+      name: [ev.name, Validators.required],
+      topic: [ev.topic, [Validators.required, Validators.maxLength(5000)]],
+      description: [ev.description, [Validators.required, Validators.maxLength(5000)]],
+      event_type: [ev.event_type, Validators.required],
+      date: [ev.date, Validators.required],
+      research_group_id: [ev.research_group_id, Validators.required],
+      frequence: [ev.frequence, Validators.required],
+      duration: [ev.duration, Validators.required],
+      state: [ev.state, Validators.required],
+      address: [ev.address, Validators.required],
+      latitude: [ev.latitude],
+      longitude: [ev.longitude]
     });
   }
+
   get name() { return this.eventForm.get('name'); }
   get topic() { return this.eventForm.get('topic'); }
   get description() { return this.eventForm.get('description'); }
@@ -161,12 +172,4 @@ export class AddEventComponent implements OnInit, AfterContentInit, OnDestroy {
   get latitude() { return this.eventForm.get('latitude'); }
   get longitude() { return this.eventForm.get('longitude'); }
   get address() { return this.eventForm.get('address'); }
-
-  setCoords($event: MouseEvent) {
-    this.latitude.setValue($event.coords.lat);
-    this.longitude.setValue($event.coords.lng);
-    this.latitude.markAsDirty();
-    this.longitude.markAsDirty();
-  }
-
 }
